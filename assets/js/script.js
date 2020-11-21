027fb81e5a92e1254acb82cfd16ac5899f2e81ce
mayurS <mayurs@prdxn.com>
2020-11-21 14:02:00 +0530

    delete card script added.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,25 @@
 var productName = document.querySelector('.product-name');
 var productQuantity = document.querySelector('.product-quantity');
-var clearAll = document.querySelector('.clear-all');
+var clearAll = document.querySelector('.clear');
 var saveProduct = document.querySelector('.save');
 var productList = document.querySelector('.product-list');
 
+
+/* Display Existing Products from local storage */
+function fetchExistingProducts() {
+  var products = getProductFromLocal();
+  products.forEach((existingProduct) => {
+    displayProducts(existingProduct.name, existingProduct.quantity);
+  });
+}
+
+fetchExistingProducts(); 
 
 saveProduct.addEventListener('click', function(e) {
   e.preventDefault();
   addProduct(productName, productQuantity);
 });
 
-
-clearAll.addEventListener('click', function() {
-  localStorage.clear();
-});
 
 /* Fetch Existing Products from local storage */
 function getProductFromLocal() {
@@ -33,21 +39,15 @@ function addProduct(name, quantity) {
     "quantity": quantity.value,
   }
 
-  var productArray = getProductFromLocal();
+  var productArray = getProductFromLocal(); 
   productArray.push(newProduct);
   localStorage.setItem('products', JSON.stringify(productArray));
-  displayProducts(newProduct.name, newProduct.quantity);
+  productList.innerHTML = '';
+  fetchExistingProducts(); 
+  // displayProducts(newProduct.name, newProduct.quantity);
+  document.querySelector('.product-form').reset();
 }
-
-/* Display Existing Products from local storage */
-function fetchExistingProducts() {
-  var products = getProductFromLocal();
-  products.forEach((existingMember) => {
-    displayProducts(existingMember.name, existingMember.quantity);
-  });
-}
-
-fetchExistingProducts();
+ 
 
 /* Create and Display product card  */
 function displayProducts(name, quantity) {
@@ -56,17 +56,12 @@ function displayProducts(name, quantity) {
   productCard.innerHTML = `
   <ul class="product-details">
     <li>
-      <figure class="product-image">
-        <img src="https://source.unsplash.com/featured/?${name}"  alt="${name}">
-      </figure>
-    </li>
-    <li>
       <span class="label-product-name">product name :</span>
-      <span id="product-name">${name}</span>
+      <span class="product-name">${name}</span>
     </li>
     <li>
       <span class="label-product-quantity">quantity :</span>
-      <span id="product-quantity">${quantity}</span>
+      <span class="product-quantity">${quantity}</span>
     </li>
     <li>
       <input type="button" class="btn edit" value="edit" />
@@ -75,4 +70,28 @@ function displayProducts(name, quantity) {
   </ul>
   `;
   productList.appendChild(productCard);
+  addDeleteFunction();
 };
+
+
+// Add Event Listener to delete button.
+function addDeleteFunction() {
+  document.querySelectorAll('.delete').forEach((element) => {
+    element.addEventListener('click', deleteCard);
+  });
+}
+
+function deleteCard(ele) {
+  var productArray = getProductFromLocal();
+  productArray.splice(productArray.findIndex(product => 
+    product.name === ele.path[2].children[0].lastElementChild.innerText.toLowerCase()), 1);
+  ele.path[3].remove();
+  localStorage.setItem('products', JSON.stringify(productArray));
+}
+
+/* Clear local storage and Product cards */ 
+clearAll.addEventListener('click', function(e) {
+  e.preventDefault();
+  localStorage.clear();
+  productList.innerHTML = '';
+});
